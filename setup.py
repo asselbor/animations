05c1639323a5a568eb5dcc5d6a15b008a7dbafe3
asselbor <thibault.asselborn@epfl.ch>
2018-11-20 16:36:51 +0100

    cleaning

---
+++ setup.py
@@ -0,0 +1,15 @@
+ #!/usr/bin/env python
+ # -*- coding: utf-8 -*-
+
+import os
+from distutils.core import setup
+import glob
+
+setup(name='animations',
+      version='0.0.1',
+      license='ISC',
+      description='A library containing animations for NAO robot',
+      author='Thibault Asselborn',
+      author_email='thibault.asselborn@epfl.cg',
+      packages=["animations"],
+)
